26c9589765322eaa08b7ae907f0dd0922692a8ed
kaswhy <sxshx818@naver.com>
2025-11-14 18:56:32 +0900

    init(#0/*): 프로젝트 초기 설정

---
+++ src/main/java/org/sopt/ticketbay/TicketbayApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.ticketbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketbayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketbayApplication.class, args);
+    }
+
+}
