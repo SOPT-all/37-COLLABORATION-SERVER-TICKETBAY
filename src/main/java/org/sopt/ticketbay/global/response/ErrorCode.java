2444168b0745d699189dcff06e8a30bb486e7095
kaswhy <sxshx818@naver.com>
2025-11-14 19:17:49 +0900

    feat(#1/global): 상태 코드 관련 인터페이스 정의

---
+++ src/main/java/org/sopt/ticketbay/global/response/ErrorCode.java
@@ -0,0 +1,5 @@
+package org.sopt.ticketbay.global.response;
+
+public interface ErrorCode extends ResponseCode {
+
+}
---
+++ src/main/java/org/sopt/ticketbay/global/response/SuccessCode.java
@@ -0,0 +1,5 @@
+package org.sopt.ticketbay.global.response;
+
+public interface SuccessCode extends ResponseCode {
+
+}
