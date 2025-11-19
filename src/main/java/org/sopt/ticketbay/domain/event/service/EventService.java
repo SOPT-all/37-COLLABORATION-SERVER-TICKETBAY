5b762959c8bedc66f0df1b3fc047295fb384bc65
Choi Min Young <codingmy@naver.com>
2025-11-20 02:32:31 +0900

    feat(#10/event): 인기 랭킹 이벤트 리스트 조회 service 메서드 추가

--- src/main/java/org/sopt/ticketbay/domain/event/service/EventService.java
+++ src/main/java/org/sopt/ticketbay/domain/event/service/EventService.java
@@ -1,7 +1,10 @@
 package org.sopt.ticketbay.domain.event.service;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.ticketbay.domain.event.domain.Event;
 import org.sopt.ticketbay.domain.event.repository.EventRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -10,4 +13,7 @@ public class EventService {
 
     private final EventRepository eventRepository;
 
+    public Page<Event> getTopEvents(int page, int size) {
+        return eventRepository.findTopEventsByViewCount(PageRequest.of(page, size));
+    }
 }
