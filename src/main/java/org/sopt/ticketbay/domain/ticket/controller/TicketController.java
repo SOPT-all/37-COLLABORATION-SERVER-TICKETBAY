1a4f5b12604bd4bf9ed39e1eddecae73f695004e
kaswhy <sxshx818@naver.com>
2025-11-20 01:57:38 +0900

    feat(#6/ticket): 티켓 상세 조회 컨트롤러 구현

--- src/main/java/org/sopt/ticketbay/domain/ticket/controller/TicketController.java
+++ src/main/java/org/sopt/ticketbay/domain/ticket/controller/TicketController.java
@@ -1,7 +1,15 @@
 package org.sopt.ticketbay.domain.ticket.controller;
 
+import static org.sopt.ticketbay.domain.ticket.controller.message.TicketSuccessCode.TICKET_DETAIL_RETRIEVED_SUCCESS;
+
 import lombok.RequiredArgsConstructor;
+import org.sopt.ticketbay.domain.ticket.controller.dto.response.TicketDetailResponse;
 import org.sopt.ticketbay.domain.ticket.service.TicketService;
+import org.sopt.ticketbay.domain.ticket.service.dto.response.TicketResult;
+import org.sopt.ticketbay.global.response.dto.ApiResponseBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class TicketController {
 
     private final TicketService ticketService;
+
+    @GetMapping("/tickets/{ticketId}")
+    public ResponseEntity<ApiResponseBody<TicketDetailResponse, Void>> getTicketDetail(
+        @PathVariable Long ticketId
+    ) {
+        TicketResult ticketResult = ticketService.getTicketDetail(ticketId);
+        TicketDetailResponse resonse = TicketDetailResponse.from(ticketResult);
+
+        return ResponseEntity.ok(ApiResponseBody.ok(TICKET_DETAIL_RETRIEVED_SUCCESS, resonse));
+    }
 
 }
