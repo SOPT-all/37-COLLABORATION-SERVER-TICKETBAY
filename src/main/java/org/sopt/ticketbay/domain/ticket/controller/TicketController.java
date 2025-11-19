1298159a9f7680b115d663b2bfbddd3b6f31a364
kaswhy <sxshx818@naver.com>
2025-11-20 00:20:45 +0900

    feat(#5/ticket): 티켓 목록 조회 컨트롤러 구현

--- src/main/java/org/sopt/ticketbay/domain/ticket/controller/TicketController.java
+++ src/main/java/org/sopt/ticketbay/domain/ticket/controller/TicketController.java
@@ -1,8 +1,20 @@
 package org.sopt.ticketbay.domain.ticket.controller;
 
+import static org.sopt.ticketbay.domain.ticket.controller.message.TicketSuccessCode.TICKET_LIST_RETRIEVED_SUCCESS;
+
+import java.time.LocalDate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.sopt.ticketbay.domain.ticket.controller.dto.response.TicketListResponse;
+import org.sopt.ticketbay.domain.ticket.controller.dto.response.TicketResponse;
 import org.sopt.ticketbay.domain.ticket.service.TicketService;
+import org.sopt.ticketbay.domain.ticket.service.dto.response.TicketResult;
+import org.sopt.ticketbay.global.response.dto.ApiResponseBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1")
@@ -11,5 +23,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class TicketController {
 
     private final TicketService ticketService;
+
+    @GetMapping("/events/{eventId}/tickets")
+    public ResponseEntity<ApiResponseBody<TicketListResponse, Void>> getTicketList(
+        @PathVariable Long eventId,
+        @RequestParam(required = false) LocalDate date
+    ) {
+        List<TicketResult> ticketResults = ticketService.getTickets(eventId, date);
+
+        TicketListResponse response  = TicketListResponse.from(
+            ticketResults.stream()
+            .map(TicketResponse::from)
+            .toList()
+        );
+
+        return ResponseEntity.ok(ApiResponseBody.ok(TICKET_LIST_RETRIEVED_SUCCESS, response));
+    }
 
 }
