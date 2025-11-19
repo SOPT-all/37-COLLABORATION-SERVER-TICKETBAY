4ea003d72df8a7b370e20b77c35c38c6a7ff3cfb
kaswhy <sxshx818@naver.com>
2025-11-20 00:20:15 +0900

    feat(#5/event): 티켓 목록 조회 서비스 구현

--- src/main/java/org/sopt/ticketbay/domain/ticket/service/TicketService.java
+++ src/main/java/org/sopt/ticketbay/domain/ticket/service/TicketService.java
@@ -1,7 +1,15 @@
 package org.sopt.ticketbay.domain.ticket.service;
 
+import static org.sopt.ticketbay.domain.event.domain.exception.EventErrorCode.EVENT_NOT_FOUND;
+
+import java.time.LocalDate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.sopt.ticketbay.domain.event.domain.exception.EventException;
+import org.sopt.ticketbay.domain.event.repository.EventRepository;
+import org.sopt.ticketbay.domain.ticket.repository.TicketCustomRepository;
 import org.sopt.ticketbay.domain.ticket.repository.TicketRepository;
+import org.sopt.ticketbay.domain.ticket.service.dto.response.TicketResult;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -9,5 +17,22 @@ import org.springframework.stereotype.Service;
 public class TicketService {
 
     private final TicketRepository ticketRepository;
+    private final TicketCustomRepository ticketCustomRepository;
+    private final EventRepository eventRepository;
+
+    public List<TicketResult> getTickets(Long eventId, LocalDate date) {
+        validateEvent(eventId);
+
+        return ticketCustomRepository.findTickets(eventId, date)
+            .stream()
+            .map(TicketResult::from)
+            .toList();
+    }
+
+    private void validateEvent(Long eventId) {
+        if(!eventRepository.existsById(eventId)) {
+            throw new EventException(EVENT_NOT_FOUND);
+        }
+    }
 
 }
