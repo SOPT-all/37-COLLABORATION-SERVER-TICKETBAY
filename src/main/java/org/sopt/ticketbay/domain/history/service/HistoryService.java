3779f143d216832cd7702a053850ca6ec88b09ec
Choi Min Young <codingmy@naver.com>
2025-11-20 02:14:57 +0900

    feat(#7/history): 최근 본 목록 조회 service 메서드 추가

--- src/main/java/org/sopt/ticketbay/domain/history/service/HistoryService.java
+++ src/main/java/org/sopt/ticketbay/domain/history/service/HistoryService.java
@@ -1,7 +1,10 @@
 package org.sopt.ticketbay.domain.history.service;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.ticketbay.domain.history.domain.History;
 import org.sopt.ticketbay.domain.history.repository.HistoryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -9,5 +12,9 @@ import org.springframework.stereotype.Service;
 public class HistoryService {
 
     private HistoryRepository historyRepository;
+
+    public Page<History> getRecentHistories(Long userId, int page, int size) {
+        return historyRepository.findRecentHistoryByUser(userId, PageRequest.of(page, size));
+    }
 
 }
