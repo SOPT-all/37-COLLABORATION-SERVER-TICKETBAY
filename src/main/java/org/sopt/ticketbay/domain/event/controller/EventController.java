8a6335db534c5219885b02807ec3742048047f5e
Choi Min Young <codingmy@naver.com>
2025-11-20 02:32:48 +0900

    feat(#10/event): 인기 랭킹 이벤트 리스트 조회 controller 구현

--- src/main/java/org/sopt/ticketbay/domain/event/controller/EventController.java
+++ src/main/java/org/sopt/ticketbay/domain/event/controller/EventController.java
@@ -1,9 +1,18 @@
 package org.sopt.ticketbay.domain.event.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.ticketbay.domain.event.controller.dto.response.EventListResponse;
+import org.sopt.ticketbay.domain.event.domain.Event;
 import org.sopt.ticketbay.domain.event.service.EventService;
+import org.sopt.ticketbay.global.response.dto.ApiResponseBody;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.sopt.ticketbay.domain.event.controller.message.EventSuccessCode.EVENT_TOP_LIST_RETRIEVED_SUCCESS;
 
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
@@ -12,4 +21,16 @@ public class EventController {
 
     private final EventService eventService;
 
+    @GetMapping("event/top")
+    public ResponseEntity<ApiResponseBody<EventListResponse, Void>> getTopEvents(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Page<Event> topEventsPage = eventService.getTopEvents(page, size);
+
+        EventListResponse response = EventListResponse.fromPage(topEventsPage);
+
+        return ResponseEntity.ok(ApiResponseBody.ok(EVENT_TOP_LIST_RETRIEVED_SUCCESS, response));
+    }
+}
 }
