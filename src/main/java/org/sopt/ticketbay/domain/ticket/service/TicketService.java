78311c4c642d74d0f1faa214cfb856a68f0e23e7
kaswhy <sxshx818@naver.com>
2025-11-20 01:56:46 +0900

    feat(#6/ticket): 티켓 상세 조회 서비스 구현

--- src/main/java/org/sopt/ticketbay/domain/ticket/service/TicketService.java
+++ src/main/java/org/sopt/ticketbay/domain/ticket/service/TicketService.java
@@ -1,7 +1,13 @@
 package org.sopt.ticketbay.domain.ticket.service;
 
+import static org.sopt.ticketbay.domain.ticket.domain.exception.TicketErrorCode.TICKET_NOT_FOUND;
+
 import lombok.RequiredArgsConstructor;
+import org.sopt.ticketbay.domain.ticket.domain.Ticket;
+import org.sopt.ticketbay.domain.ticket.domain.exception.TicketException;
 import org.sopt.ticketbay.domain.ticket.repository.TicketRepository;
+import org.sopt.ticketbay.domain.ticket.service.dto.response.TicketResult;
+import org.sopt.ticketbay.global.s3.S3Service;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -9,5 +15,14 @@ import org.springframework.stereotype.Service;
 public class TicketService {
 
     private final TicketRepository ticketRepository;
+    private final S3Service s3Service;
+
+    public TicketResult getTicketDetail(Long ticketId) {
+        Ticket ticket = ticketRepository.findById(ticketId)
+            .orElseThrow(() -> new TicketException(TICKET_NOT_FOUND));
+        String imageUrl = s3Service.generatePresignedUrl(ticket.getSeat().getImageKey());
+
+        return TicketResult.of(ticket, imageUrl);
+    }
 
 }
