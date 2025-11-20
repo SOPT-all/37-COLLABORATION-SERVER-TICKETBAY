b2bcc1e67390e8030dd6a41079ceee3d8f6f4ac3
kaswhy <sxshx818@naver.com>
2025-11-20 17:35:39 +0900

    refactor(#6/ticket): 이벤트 검증 로직 EventValidator로 분리

--- src/main/java/org/sopt/ticketbay/domain/ticket/service/TicketService.java
+++ src/main/java/org/sopt/ticketbay/domain/ticket/service/TicketService.java
@@ -1,12 +1,9 @@
 package org.sopt.ticketbay.domain.ticket.service;
 
-import static org.sopt.ticketbay.domain.event.domain.exception.EventErrorCode.EVENT_NOT_FOUND;
-
 import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.sopt.ticketbay.domain.event.domain.exception.EventException;
-import org.sopt.ticketbay.domain.event.repository.EventRepository;
+import org.sopt.ticketbay.domain.event.validator.EventValidator;
 import org.sopt.ticketbay.domain.ticket.repository.TicketCustomRepository;
 import org.sopt.ticketbay.domain.ticket.repository.TicketRepository;
 import org.sopt.ticketbay.domain.ticket.service.dto.response.TicketResult;
@@ -18,21 +15,15 @@ public class TicketService {
 
     private final TicketRepository ticketRepository;
     private final TicketCustomRepository ticketCustomRepository;
-    private final EventRepository eventRepository;
+    private final EventValidator eventValidator;
 
     public List<TicketResult> getTickets(Long eventId, LocalDate date) {
-        validateEvent(eventId);
+        eventValidator.validateEvent(eventId);
 
         return ticketCustomRepository.findTickets(eventId, date)
             .stream()
             .map(TicketResult::from)
             .toList();
-    }
-
-    private void validateEvent(Long eventId) {
-        if(!eventRepository.existsById(eventId)) {
-            throw new EventException(EVENT_NOT_FOUND);
-        }
     }
 
 }
