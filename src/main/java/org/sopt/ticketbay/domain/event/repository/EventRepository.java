8d0e0c0c20b1a1080d6ffd8f0f9ea0b1e507b43f
Choi Min Young <codingmy@naver.com>
2025-11-21 17:04:50 +0900

    refactor(#10/event): jpa 상속 삭제

--- src/main/java/org/sopt/ticketbay/domain/event/repository/EventRepository.java
+++ src/main/java/org/sopt/ticketbay/domain/event/repository/EventRepository.java
@@ -3,8 +3,7 @@ package org.sopt.ticketbay.domain.event.repository;
 import org.sopt.ticketbay.domain.event.domain.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface EventRepository {
     Page<Event> findTopEventsByViewCount(Pageable pageable);
 }
