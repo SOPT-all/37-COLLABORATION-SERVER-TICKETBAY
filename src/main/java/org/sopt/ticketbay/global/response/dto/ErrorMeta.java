ffcf67af302520c79d64091640e69191e9537f80
kaswhy <sxshx818@naver.com>
2025-11-21 18:15:07 +0900

    feat(#13/*): ErrorMeta 스웨거 명세 추가

--- src/main/java/org/sopt/ticketbay/global/response/dto/ErrorMeta.java
+++ src/main/java/org/sopt/ticketbay/global/response/dto/ErrorMeta.java
@@ -1,9 +1,14 @@
 package org.sopt.ticketbay.global.response.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.Instant;
 
 public record ErrorMeta(
+
+    @Schema(description = "에러가 발생한 API 경로입니다.")
     String path,
+
+    @Schema(description = "에러가 발생한 시간입니다.")
     Instant timestamp
 ) {
 
