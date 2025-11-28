c2839f29014f2cd10648843ec4530cf5633a75d4
min0 <97686638+codingmy@users.noreply.github.com>
2025-11-28 16:45:45 +0900

    setting (#21/*) start.sh application.yml 프로필 세팅 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,11 +2,14 @@
 
 cd /home/ubuntu/ticketbay
 
+# env.list 파일 불러오기 (GitHub Actions에서 받아온 Secrets값들)
+export $(cat env.list | xargs)
+
 # 기존 프로세스 종료
 PID=$(lsof -ti :8080)
 if [ -n "$PID" ]; then
   kill -9 $PID
 fi
 
-# 새 버전 실행
-nohup java -jar *.jar > app.log 2>&1 &
+# 새 버전 실행 (환경변수 적용시키는 것 포함)
+nohup java -jar *.jar --spring.profiles.active=$SPRING_PROFILES_ACTIVE > app.log 2>&1 &
